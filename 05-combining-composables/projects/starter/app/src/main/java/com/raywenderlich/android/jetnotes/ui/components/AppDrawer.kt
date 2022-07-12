f243414e40eb68f07ac1f335e4da159bcdde2469
Moses Asiago <mozzeago@gmail.com>
2022-07-12 05:06:09 +0300

    finished AppDrawer and ScreenNavigation Composables

---
+++ 05-combining-composables/projects/starter/app/src/main/java/com/raywenderlich/android/jetnotes/ui/components/AppDrawer.kt
@@ -0,0 +1,109 @@
+package com.raywenderlich.android.jetnotes.ui.components
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.jetnotes.theme.JetNotesTheme
+
+@Composable
+private fun AppDrawerHeader() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Image(
+            imageVector = Icons.Filled.Menu, contentDescription = "Drawer Header Icon",
+            colorFilter = ColorFilter.tint(MaterialTheme.colors.onSurface),
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(text = "JetNotes", modifier = Modifier.align(alignment = Alignment.CenterVertically))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppDrawerPreview() {
+    JetNotesTheme {
+        AppDrawerHeader()
+    }
+}
+
+@Composable
+private fun ScreenNavigationButton(
+    icon: ImageVector,
+    label: String,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    val colors = MaterialTheme.colors
+    //Define alpha for the image  for two different states of the button :selected/unselected.
+    val imageAlpha = if (isSelected) {
+        1f
+    } else {
+        0.6f
+    }
+    val textColor = if (isSelected) {
+        colors.primary
+    } else {
+        colors.onSurface.copy(0.6f)
+    }
+    val backgroundColo = if (isSelected) {
+        colors.primary.copy(0.12f)
+    } else {
+        colors.surface
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp), color = backgroundColor,
+        shape = MaterialTheme.shapes.small
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable(onClick = onClick)
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            Image(
+                imageVector = icon,
+                contentDescription = "Screen Navigation Button",
+                colorFilter = ColorFilter.tint(textColor),
+                alpha = imageAlpha
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = label,
+                style = MaterialTheme.typography.body2,
+                color = textColor,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScreenNavigationButtonPreview() {
+    JetNotesTheme {
+        ScreenNavigationButton(
+            icon = Icons.Filled.Home,
+            label = "Notes",
+            isSelected = true,
+            onClick = {})
+    }
+}
