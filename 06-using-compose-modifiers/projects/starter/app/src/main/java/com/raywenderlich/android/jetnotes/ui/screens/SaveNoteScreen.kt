c7c791656474a3a2fee0509857b744d0d7106a12
Moses Asiago <mozzeago@gmail.com>
2022-07-15 07:44:25 +0300

    Note ColorPicker composable complete, end of chapter

--- 06-using-compose-modifiers/projects/starter/app/src/main/java/com/raywenderlich/android/jetnotes/ui/screens/SaveNoteScreen.kt
+++ 06-using-compose-modifiers/projects/starter/app/src/main/java/com/raywenderlich/android/jetnotes/ui/screens/SaveNoteScreen.kt
@@ -1,21 +1,45 @@
 package com.raywenderlich.android.jetnotes.ui.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.jetnotes.domain.model.ColorModel
 import com.raywenderlich.android.jetnotes.ui.components.NoteColor
 import com.raywenderlich.android.jetnotes.util.fromHex
+
+@Composable
+private fun ColorPicker(
+    colors: List<ColorModel>,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Color Picker",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold, modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            items(colors.size) { itemIndex ->
+                val color = colors[itemIndex]
+                ColorItem(color = color, onColorSelect = onColorSelect)
+            }
+        }
+    }
+}
+
 
 @Composable
 fun ColorItem(
@@ -49,6 +73,20 @@ fun ColorItem(
 @Composable
 fun ColorItemPreview() {
     ColorItem(color = ColorModel.DEFAULT) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ColorPickerPreview() {
+    ColorPicker(
+        colors = listOf(
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT
+        )
+    ) {
 
     }
 }
